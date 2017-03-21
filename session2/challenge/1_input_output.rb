6c4b4bb6199618a8fdcaef599687a08157edc45a
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-21 19:55:18 +0000

    Completed session2/challenge/1.input_output.rb

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -7,6 +7,21 @@
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
 
+
 def sum_difference_product
-  # your code goes here
+  x , y = gets.split.map { |s| s.to_i }
+  puts x + y ; puts x - y ; puts x * y
 end
+
+
+=begin
+	
+Take two numbers as input, 'x , y' in this case - by calling '.gets'
+
+'.split' will split the string into two seperate stings
+
+call '.map' to invoke a block, where you replace each String object with an Integer using the '.to_i' method
+
+Now simply output using 'puts' to get the sum, difference and product of the input ('x', 'y')
+	
+=end
