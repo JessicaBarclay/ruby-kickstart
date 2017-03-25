52bd1a912731720700c9375e8057b1a1561fbc6b
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-25 23:13:21 +0000

    Completed ../challenge/1_blocks.rb

--- session3/challenge/1_blocks.rb
+++ session3/challenge/1_blocks.rb
@@ -1,5 +1,23 @@
 # Write a method, reverse_map, which invokes a block on each of the elements in reverse order,
 # and returns an array of their results.
-#
+
 # reverse_map(1, 2, 3) { |i| i * 2 }      # => [6, 4, 2]
 
+
+
+
+
+# Pass multiple arguments to method with *args
+
+# Pass method a block using &block to loop over     e.g     { |i| i * 2 } 
+
+
+
+def reverse_map(*args, &block)
+	array = [*args].reverse
+	array.map!(&block)
+end
+
+
+
+p reverse_map(1, 2, 3) { |i| i * 2 }   ### ===>>>    [6, 4, 2]
