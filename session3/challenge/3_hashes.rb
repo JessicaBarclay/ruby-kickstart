ee296b13bb29b8880044effac1ce31823256f061
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-26 17:36:32 +0100

    Completed and Refactored challenges/3_hashes.rb

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,7 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(string)
+	string.downcase.split.each_with_object(Hash.new(0)){|word, count| count[word] += 1 }
+end
