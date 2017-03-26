0c924a9bae9c925cc642e145229b04a5725c4502
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-26 18:12:23 +0100

    ../challenge/4_hashes.rb

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,12 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+
+def first_pos(string)
+	hash = {}
+	string.split.map.with_index do |key, index| # loop the split string using map with index. 
+		hash[key] ||= index # print the key or the index, which ever comes first
+	end
+	  hash
+end
