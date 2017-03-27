ec3627cc508b7569bc0e34e066d3ed1a56251a42
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-03-27 08:46:55 +0100

    Completed session3/challenge/16_hash.rb

--- session3/challenge/16_hash.rb
+++ session3/challenge/16_hash.rb
@@ -15,3 +15,7 @@
 # head = {:data => 6, :next => head}
 # print_list head                    # >> "6\n5\n4\n3\n2\n1\n"
 
+def print_list(linked_list)
+  while linked_list
+  p linked_list[:data] ; linked_list = linked_list[:next] end
+end          
